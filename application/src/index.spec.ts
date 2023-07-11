79b7e3e609785c1ae956ac699c3455d3d14e42d3
rfrad <remi.fradet@gmail.com>
2023-07-12 00:14:20 +0200

    Remove unused files

--- application/src/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'jasmine';
-import { Printer } from ".";
-
-describe('Printer', () => {
-    it('should print hello', () => {
-        const printer: Printer = new Printer();
-        expect(printer.echo()).toEqual('hello');
-    });
-});
--- application/src/index.ts
+++
@@ -1,5 +0,0 @@
-export class Printer {
-    public echo(): string {
-        return "hello";
-    }
-}
