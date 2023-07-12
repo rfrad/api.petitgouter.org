a020aaf05d9f1b5f0383a0646f05ca45ba52fd75
rfrad <remi.fradet@gmail.com>
2023-07-12 18:22:10 +0200

    Fix the authorizer to return a valid format

--- application/src/authorizers/open-to-all.spec.ts
+++ application/src/authorizers/open-to-all.spec.ts
@@ -1,13 +1,20 @@
 import 'jasmine';
 import { authorize } from './open-to-all';
-import { APIGatewayAuthorizerEvent, AuthResponse, PolicyDocument } from 'aws-lambda';
+import { APIGatewayAuthorizerEvent, AuthResponse } from 'aws-lambda';
 
 describe('OpenToAll authorizer', () => {
     it('should return the authorizer values', async () => {
         const result: AuthResponse = await authorize(<APIGatewayAuthorizerEvent><unknown>undefined);
         expect(result).toEqual({
-            policyDocument: {} as PolicyDocument,
-            principalId: ''
+            policyDocument: {
+                Version: '2012-10-17',
+                Statement: [{
+                    Action: 'execute-api:Invoke',
+                    Effect: 'Allow',
+                    Resource: "arn:aws:execute-api:*:*:*"
+                }]
+            },
+            principalId: 'anyone'
         });
     });
 });
--- application/src/authorizers/open-to-all.ts
+++ application/src/authorizers/open-to-all.ts
@@ -1,15 +1,22 @@
-import { AuthResponse, APIGatewayAuthorizerEvent, PolicyDocument } from 'aws-lambda'
+import { AuthResponse, APIGatewayAuthorizerEvent } from 'aws-lambda'
 
 export const authorize = async (event: APIGatewayAuthorizerEvent): Promise<AuthResponse> => {
     const authorizer: OpenToAll = new OpenToAll();
-    return authorizer.authorize(event);
+    return await authorizer.authorize(event);    
 }
 
 class OpenToAll {
     async authorize(event: APIGatewayAuthorizerEvent): Promise<AuthResponse> {
         return {
-            policyDocument: {} as PolicyDocument,
-            principalId: ''
+            policyDocument: {
+                Version: '2012-10-17',
+                Statement: [{
+                    Action: 'execute-api:Invoke',
+                    Effect: 'Allow',
+                    Resource: "arn:aws:execute-api:*:*:*"
+                }]
+            },
+            principalId: 'anyone'
         };
     }
 }
